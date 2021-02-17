078e39f1140f640c1dc855a967560649c0d2c3f1
JoshSevy <joshuasevy@outlook.com>
2021-02-17 15:55:37 -0700

    Connect posts data to components to save posts and display to all needed comps

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-post-create',
@@ -6,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-  enteredValue = "";
-  newPost = "NO CONTENT";
+  enteredContent = "";
+  enteredTitle = "";
+  @Output() postCreated = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  onAddPost(postInput: string) {
-    this.newPost = postInput;
+  onAddPost() {
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent,
+    }
+    this.postCreated.emit(post);
   }
 
 }
