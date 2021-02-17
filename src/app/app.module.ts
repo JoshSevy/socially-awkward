1c28a135b7d37c299bf45055d375add65de5752d
JoshSevy <joshuasevy@outlook.com>
2021-02-17 15:13:57 -0700

    Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule} from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -21,6 +24,7 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
