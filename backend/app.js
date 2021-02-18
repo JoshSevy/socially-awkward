66f2af1ecfdac603db237b2f81b7c8879c843cc6
JoshSevy <joshuasevy@outlook.com>
2021-02-18 14:29:15 -0700

    Connect api to post and get from db

--- backend/app.js
+++ backend/app.js
@@ -35,22 +35,20 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added successfully"
   });
 });
 
 app.get("/api/posts",(req, res, next) => {
-  const posts = [
-    {id: '00001', title: "test1", content: "server/api test 1" },
-    {id: '00002', title: "test2", content: "server/api test 2" },
-    {id: '00003', title: "test3", content: "server/api test 3" }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully!',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
