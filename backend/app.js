4c4462162814034eed0926cbf8f6578a10e7c4c5
JoshSevy <joshuasevy@outlook.com>
2021-02-17 19:34:24 -0700

    Add posts to express server to be able to handle api

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from express');
+app.use("/api/posts",(req, res, next) => {
+  const posts = [
+    {id: '00001', title: "test1", content: "server/api test 1" },
+    {id: '00002', title: "test2", content: "server/api test 2" },
+    {id: '00003', title: "test3", content: "server/api test 3" }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
