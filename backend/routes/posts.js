445b7aebe2d0625e102f48cf3e6da0dd0731b60c
JoshSevy <joshuasevy@outlook.com>
2021-02-25 19:08:47 -0700

    Add auth interceptor to project

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 
 const Post = require('../models/post');
+const checkAuth = require('../middleware/check-auth');
 
 const router = express.Router();
 
@@ -28,7 +29,7 @@ const storage = multer.diskStorage({
 });
 
 router.post(
-  "",
+  "", checkAuth,
   multer({storage: storage}).single("image"),
   (req, res, next) => {
     const url = req.protocol + '://' + req.get("host");
@@ -50,7 +51,7 @@ router.post(
 });
 
 router.put(
-  "/:id",
+  "/:id", checkAuth,
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
     let imagePath = req.body.imagePath;
@@ -103,7 +104,7 @@ router.get("/:id", (req, res, next) => {
   })
 })
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", checkAuth, (req, res, next) => {
   Post.deleteOne({_id: req.params.id})
     .then(result => {
       console.log(result);
